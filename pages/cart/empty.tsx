35072436e68eead4be592fb9e3944572f22ea76f
Daniel Unapanta <dunapanta@outlook.es>
2022-06-22 22:01:48 -0500

    Add no items on cart page

---
+++ pages/cart/empty.tsx
@@ -0,0 +1,40 @@
+import NextLink from "next/link";
+import { RemoveShoppingCartOutlined } from "@mui/icons-material";
+import { Box, Link, Typography } from "@mui/material";
+import { ShopLayout } from "components/layouts";
+
+const EmptyPage = () => {
+  return (
+    <ShopLayout
+      title="Carrito vacío"
+      pageDescription="No hay artículos en el carrito"
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="calc(100vh - 200px)"
+        sx={{ flexDirection: { xs: "column", md: "row" } }}
+      >
+        <RemoveShoppingCartOutlined sx={{ fontSize: 100 }} />
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Typography
+            variant="h4"
+            component="h4"
+            fontSize={25}
+            fontWeight={200}
+          >
+            Carrito vacío
+          </Typography>
+          <NextLink href="/" passHref>
+            <Link typography="h4" color="secondary">
+              Regresar
+            </Link>
+          </NextLink>
+        </Box>
+      </Box>
+    </ShopLayout>
+  );
+};
+
+export default EmptyPage;
