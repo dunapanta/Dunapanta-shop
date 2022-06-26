2f8dbbef81ff234e5d3a64b1e1a2d76229a84dce
Daniel Unapanta <dunapanta@outlook.es>
2022-06-25 22:47:50 -0500

    Add shearch query param for gender

--- database/index.ts
+++ database/index.ts
@@ -1,3 +1,4 @@
 export * as db from "database/db";
+export * from "database/constants";
 
 export * as seedDatabase from "database/products";
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { db } from "database";
+import { db, SHOP_CONSTANTS } from "database";
 import { IProduct } from "interfaces";
 import { Product } from "models";
 
@@ -23,9 +23,16 @@ export default function handler(
 }
 
 const getProducts = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { gender = "all" } = req.query;
+
+  let condition = {};
+
+  if (gender !== "all" && SHOP_CONSTANTS.validGenders.includes(`${gender}`)) {
+    condition = { gender };
+  }
   await db.connect();
 
-  const products = await Product.find()
+  const products = await Product.find(condition)
     .select("title images price inStock slug -_id")
     .lean();
 
