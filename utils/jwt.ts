5953b556f67a4a4e338ff87db1d466d836ba1814
Daniel Unapanta <dunapanta@outlook.es>
2022-08-19 20:48:18 -0500

    Add validate-token

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -15,3 +15,23 @@ export const signToken = (_id: string, email: string) => {
     { expiresIn: "30d" }
   );
 };
+
+export const verifyToken = (token: string): Promise<string> => {
+  if (!process.env.JWT_SECRET_KEY) {
+    throw new Error("JWT SECRET is not defined");
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET_KEY || "", (err, payload) => {
+        if (err) return reject("JWT no es válido");
+
+        const { _id } = payload as { _id: string };
+
+        resolve(_id);
+      });
+    } catch (e) {
+      reject("JWT no es válido");
+    }
+  });
+};
