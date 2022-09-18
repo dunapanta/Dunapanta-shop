b4eed388af04e22b6caa2781a2240e03ad9202ab
Daniel Unapanta <dunapanta@outlook.es>
2022-09-18 11:17:12 -0500

    Add credentials for custom auth

--- notas.md
+++ notas.md
@@ -8,4 +8,10 @@
 - Agrego variables de entorno
 - `csrf-token` token para asegurar la comunicación cliente - servidor de que no fueron cambaidos los tokens
 
+## Clase 296 credentials
+- Se agregan los campos de email y password
+- Si retorna null es que no se pudo loguear
+- Si retorna un objeto es que se logueo correctamente
+- Agregar `NEXT_AUTH_SECRET` a las variables de entorno
+
 
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,37 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    // My Authentication
+    Credentials({
+      name: "Custom Login",
+      credentials: {
+        email: { label: "Correo:", type: "email", placeholder: "Correo" },
+        password: {
+          label: "Contraseña:",
+          type: "password",
+          placeholder: "Contraseña",
+        },
+      },
+      async authorize(credentials) {
+        console.log(credentials);
+        //TODO: Validate credentials on my database
+
+        return {name: "daniel", correo: "dan@mail.com", roel: "admin"};
+      },
+    }),
+    //Github
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
     }),
-    // ...add more providers here
   ],
+  //callbacks
+    callbacks: {
+        
+    }
 };
 export default NextAuth(authOptions);
