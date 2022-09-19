2d310bf416f7b8ca305da38f0283164b9d5c3f07
Daniel Unapanta <dunapanta@outlook.es>
2022-09-18 20:40:13 -0500

    Refactor middleware with next auth for checkout

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,30 @@
- // middleware.ts
- import { NextResponse } from 'next/server';
- import type { NextRequest } from 'next/server';
-  
- import * as jose from 'jose'
-  
- // This function can be marked `async` if using `await` inside
- export async function middleware(request: NextRequest) {
-  
-     if (request.nextUrl.pathname.startsWith('/checkout')) {
-         // Getting cookies from the request
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+import * as jose from "jose";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/checkout")) {
+    const session = await getToken({
+      req: request,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
+
+    console.log({ session });
+
+    if (!session) {
+      const { origin, pathname } = request.nextUrl;
+
+      return NextResponse.redirect(`${origin}/auth/login?p=${pathname}`);
+    }
+
+    return NextResponse.next();
+
+    //Custom JWT
+    /*  // Getting cookies from the request
          const token = request.cookies.get('token');
          let isValidToken = false;
 
@@ -31,10 +47,11 @@
              return NextResponse.redirect(
                  new URL(`/auth/login?redirect=${pathname}`, request.url)
              );
-         }
-     }
- }
-  
- // See "Matching Paths" below to learn more
- export const config = {
-     matcher: ['/checkout/:path*'],}
+         } */
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/checkout/:path*"],
+};
