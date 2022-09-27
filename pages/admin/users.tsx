cd02bcd61bcefc540ac9e475584a8fbdebfd6823
Daniel Unapanta <dunapanta@outlook.es>
2022-09-26 20:56:06 -0500

    Add endpointo to change user role

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,21 +1,48 @@
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
-import { Grid, Typography } from "@mui/material";
+import { Grid, MenuItem, Select, Typography } from "@mui/material";
 import useSWR from "swr";
 import { AdminLayout } from "components/layouts";
 import React from "react";
 import { PeopleOutline } from "@mui/icons-material";
 
 import { IUser } from "interfaces";
+import { shopApi } from "api";
 
 const UsersPage = () => {
   const { data, error } = useSWR<IUser[]>("/api/admin/users");
 
   if (!data && !error) return <Typography variant="h4">Loading...</Typography>;
 
+  const onRoleUpdate = async (userId: string, newRole: string) => {
+    try {
+      await shopApi.put("/admin/users", { userId, role: newRole });
+    } catch (err) {
+      console.log(err);
+      alert("Error updating user role");
+    }
+  };
+
   const columns: GridColDef[] = [
     { field: "email", headerName: "Correo", width: 250 },
     { field: "name", headerName: "Nombre Completo", width: 300 },
-    { field: "role", headerName: "Rol", width: 300 },
+    {
+      field: "role",
+      headerName: "Rol",
+      width: 300,
+      renderCell: ({ row }: GridValueGetterParams) => {
+        return (
+          <Select
+            value={row.role}
+            label="Role"
+            sx={{ width: "300px" }}
+            onChange={(event) => onRoleUpdate(row.id, event.target.value)}
+          >
+            <MenuItem value="admin">Admin</MenuItem>
+            <MenuItem value="client">Client</MenuItem>
+          </Select>
+        );
+      },
+    },
   ];
 
   const rows = data!.map((user) => ({
@@ -32,7 +59,7 @@ const UsersPage = () => {
       icon={<PeopleOutline />}
     >
       <Grid container className="fadeIn">
-        <Grid item xs={12} sx={{ height: 650, width: "100%" }}>
+        <Grid item xs={12} sx={{ height: 650, width: "95%" }}>
           <DataGrid
             rows={rows}
             columns={columns}
