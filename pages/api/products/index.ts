92fb868b27f93943dcc232c56acfbce43363c8d7
Daniel Unapanta <dunapanta@outlook.es>
2022-09-29 09:16:28 -0500

    Add updateProduct endpoint

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -3,10 +3,14 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { db, SHOP_CONSTANTS } from "database";
 import { IProduct } from "interfaces";
 import { Product } from "models";
+import { isValidObjectId } from "mongoose";
 
-type Data = {
-  message: string | IProduct[];
-};
+type Data =
+  | {
+      message: string;
+    }
+  | IProduct[]
+  | IProduct;
 
 export default function handler(
   req: NextApiRequest,
@@ -15,6 +19,8 @@ export default function handler(
   switch (req.method) {
     case "GET":
       return getProducts(req, res);
+    case "PUT":
+      return updateProduct(req, res);
     default:
       return res.status(405).json({ message: "Method not allowed" });
   }
@@ -37,4 +43,42 @@ const getProducts = async (req: NextApiRequest, res: NextApiResponse) => {
   await db.disconnect();
 
   return res.status(200).json(products);
+};
+
+const updateProduct = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { _id = "", images = [] } = req.body as IProduct;
+
+  if (!isValidObjectId(_id)) {
+    return res.status(400).json({ message: "Invalid product id" });
+  }
+
+  if (images.length <= 2) {
+    return res
+      .status(400)
+      .json({ message: "You must upload at least 2 images" });
+  }
+
+  //TODO: Upload images to cloudinary
+
+  try {
+    await db.connect();
+
+    const product = await Product.findById(_id);
+    if (!product) {
+      await db.disconnect();
+      return res.status(400).json({ message: "Product not found" });
+    }
+
+    //TODO eliminar fotos en Cloudinary
+
+    await product.update(req.body);
+
+    await db.disconnect();
+
+    return res.status(200).json(product);
+  } catch (err) {
+    console.log("Error", err);
+    await db.disconnect();
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
